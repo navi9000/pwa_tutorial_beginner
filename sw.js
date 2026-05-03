6275eda2d98a3621227904cbf2c356aa4e82c92d
navi9000 <icdi7434@gmail.com>
2026-05-03 12:14:37 +0400

    add a service worker

---
+++ sw.js
@@ -0,0 +1,55 @@
+const VERSION = "v1"
+const CACHE_NAME = `period-tracker-${VERSION}`
+
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/wheel.svg",
+]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      cache.addAll(APP_STATIC_RESOURCES)
+    })(),
+  )
+})
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys()
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name)
+          }
+          return undefined
+        }),
+      )
+      await clients.claim()
+    })(),
+  )
+})
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("/"))
+    return
+  }
+
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      const cachedResponse = await cache.match(event.request.url)
+      if (cachedResponse) {
+        return cachedResponse
+      }
+      return new Response(null, { status: 404 })
+    })(),
+  )
+})
